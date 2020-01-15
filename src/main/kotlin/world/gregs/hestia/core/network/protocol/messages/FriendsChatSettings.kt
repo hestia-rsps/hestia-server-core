b7c0be128bfda37be23e9b42e4645e46d8ad8138
GregHib <gregoryhib@gmail.com>
2020-01-15 21:11:27 +0000

    Renamed `widget` to `interface`

--- src/main/kotlin/world/gregs/hestia/core/network/protocol/messages/FriendsChatSettings.kt
+++ src/main/kotlin/world/gregs/hestia/core/network/protocol/messages/FriendsChatSettings.kt
@@ -3,9 +3,9 @@ package world.gregs.hestia.core.network.protocol.messages
 import world.gregs.hestia.core.network.codec.message.Message
 
 /**
- * Friends chat setup widget actions to change players friends chat settings
+ * Friends chat setup interface actions to change players friends chat settings
  * @param entity The players entity id
- * @param hash The widget id and component id combined
+ * @param hash The interface id and component id combined
  * @param option The option selected
  */
 data class FriendsChatSettings(val entity: Int, val hash: Int, val option: Int): Message
